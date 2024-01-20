cd514db0850c107ab7189795d6cf2c7c1783a703
lishlishlish <156820333+lishlishlish@users.noreply.github.com>
2024-01-20 16:35:27 +0000

    Lish changes

--- Game states/Assets/PlayerController.cs
+++ Game states/Assets/PlayerController.cs
@@ -4,51 +4,60 @@ using System.Collections.Specialized;
 using System.Security.Cryptography;
 using System.Threading;
 using TMPro;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] public int health;
     [SerializeField] GameObject Textref;
-    TextMeshProUGUI text; 
+    TextMeshProUGUI text;
     string healthText;
     public float speed = 10.0f;
     float cameraSpeed = 2.0f;
     [SerializeField] Camera cam;
-
+    private CharacterController characterController;
+    private bool groundedPlayer;
+    private Vector3 playerVelocity;
+    public float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
     // Start is called before the first frame update
     void Start()
     {
+        characterController = GetComponent<CharacterController>();
         text = Textref.GetComponent<TextMeshProUGUI>();
         Cursor.lockState = CursorLockMode.Locked;
     }
     // Update is called once per frame
     void Update()
     {
-        Vector3 pos = transform.position;
+
+        groundedPlayer = characterController.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 pos = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         float h = cameraSpeed * Input.GetAxis("Mouse X");
         float v = -cameraSpeed * Input.GetAxis("Mouse Y");
-        if (Input.GetKey("w"))
+
+        characterController.Move(pos * Time.deltaTime * speed);
+
+        if (pos != Vector3.zero)
         {
-            pos += transform.forward * speed * Time.deltaTime;
+            transform.forward = pos;
         }
-        if (Input.GetKey("s"))
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
         {
-            pos -= transform.forward * speed * Time.deltaTime;
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
         }
-        if (Input.GetKey("d"))
-        {
-            pos += transform.right * speed * Time.deltaTime;
-        }
-        if (Input.GetKey("a"))
-        {
-            pos -= transform.right * speed * Time.deltaTime;
-        }
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        characterController.Move(playerVelocity * Time.deltaTime);
         text.SetText(health.ToString());
-        transform.position = pos;
         transform.Rotate(0, h, 0);
         cam.transform.Rotate(v, 0, 0);
-        if(health <= 0)
+        if (health <= 0)
         {
             health = 0;
             cameraSpeed = 0;
@@ -57,4 +66,3 @@ public class PlayerController : MonoBehaviour
         }
     }
 }
-
