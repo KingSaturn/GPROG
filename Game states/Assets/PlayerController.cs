ce53d73816a8c60dcc1c23500907d3060ded05bc
KingSaturn <93086586+KingSaturn@users.noreply.github.com>
2024-01-20 17:18:20 +0000

    added jumping and running

--- Game states/Assets/PlayerController.cs
+++ Game states/Assets/PlayerController.cs
@@ -13,50 +13,65 @@ public class PlayerController : MonoBehaviour
     [SerializeField] GameObject Textref;
     TextMeshProUGUI text;
     string healthText;
-    public float speed = 10.0f;
-    float cameraSpeed = 2.0f;
+
+    public float speed = 8.0f;
+    public float runspeed = 12.0f;
+    public float jumpHeight = 8.0f;
+    private float gravityValue = 20f;
     [SerializeField] Camera cam;
+    public float cameraSpeed = 2.0f;
+    public float lookXLimit = 35.0f;
+
     private CharacterController characterController;
-    private bool groundedPlayer;
-    private Vector3 playerVelocity;
-    public float jumpHeight = 1.0f;
-    private float gravityValue = -9.81f;
+    Vector3 moveDirection = Vector3.zero;
+    float rotationX = 0;
+
+    [HideInInspector]
+    public bool canMove = true;
     // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        //HUD Text
         text = Textref.GetComponent<TextMeshProUGUI>();
+        //lock cursor
         Cursor.lockState = CursorLockMode.Locked;
     }
     // Update is called once per frame
     void Update()
     {
 
-        groundedPlayer = characterController.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float curSpeedX = canMove ? (isRunning ? runspeed : speed) * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? (isRunning ? runspeed : speed) * Input.GetAxis("Horizontal") : 0;
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+        if(Input.GetButton("Jump") && canMove && characterController.isGrounded)
         {
-            playerVelocity.y = 0f;
+            moveDirection.y = jumpHeight;
+        }
+        else
+        {
+            moveDirection.y = movementDirectionY;
+        }
+        if(!characterController.isGrounded)
+        {
+            moveDirection.y -= gravityValue * Time.deltaTime;
+        }
+        characterController.Move(moveDirection * Time.deltaTime);
+        if(canMove)
+        {
+            rotationX += -Input.GetAxis("Mouse Y") * cameraSpeed;
+            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            cam.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * cameraSpeed, 0);
         }
 
-        Vector3 pos = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        float h = cameraSpeed * Input.GetAxis("Mouse X");
-        float v = -cameraSpeed * Input.GetAxis("Mouse Y");
 
-        characterController.Move(pos * Time.deltaTime * speed);
-
-        if (pos != Vector3.zero)
-        {
-            transform.forward = pos;
-        }
-        if (Input.GetButtonDown("Jump") && groundedPlayer)
-        {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-        }
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        characterController.Move(playerVelocity * Time.deltaTime);
         text.SetText(health.ToString());
-        transform.Rotate(0, h, 0);
-        cam.transform.Rotate(v, 0, 0);
         if (health <= 0)
         {
             health = 0;
